51cecfd111e4b2eb10f638427935ef0acdb30839
DanyaGTx <raxub007@gmail.com>
2023-05-30 18:05:25 +0300

    added welcome and project section

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const downloadCV = () => {
+    const resumeLink = "https://docs.google.com/document/d/1gLWSUHRE7Zy72y7suqyZwnPg-m_l6C4-/export?format=pdf"
+    window.location.href= resumeLink
+}
+
+const contactRedirect = () => {
+    const tgLink = "https://t.me/teresheyn"
+    window.location.href= tgLink
+}
+
