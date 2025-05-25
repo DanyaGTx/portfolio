cfc53eef7a91250a884a84f2812b256987904e99
DanyaGTx <raxub007@gmail.com>
2025-05-25 16:27:32 +0200

    add: new project and update resume link

--- js/index.js
+++ js/index.js
@@ -1,47 +1,48 @@
 const downloadCV = () => {
-    const resumeLink = "https://drive.usercontent.google.com/uc?id=1loLTJ44h8NaJ_ex_MQkDIBxkSRcCmyiR&authuser=0&export=download"
-    window.location.href= resumeLink
-}
+  const resumeLink =
+    "https://drive.usercontent.google.com/uc?id=1uA2D8SH2IB1xTFJnzCO_jAQu3i4xQy-w&authuser=0&export=download";
+  window.location.href = resumeLink;
+};
 
-const projectItems = document.querySelectorAll('.projects-list-item');
-const overlayItems = document.querySelectorAll('.projects-list-item-overlay');
+const projectItems = document.querySelectorAll(".projects-list-item");
+const overlayItems = document.querySelectorAll(".projects-list-item-overlay");
 let activeIndex = null;
 
 for (let i = 0; i < projectItems.length; i++) {
-  projectItems[i].addEventListener('click', (event) => {
+  projectItems[i].addEventListener("click", (event) => {
     event.stopPropagation(); // Prevents the click event from bubbling up to the document level
 
     if (activeIndex !== null && activeIndex !== i) {
       overlayItems[activeIndex].style.opacity = 0;
-      overlayItems[activeIndex].style.visibility = 'hidden';
+      overlayItems[activeIndex].style.visibility = "hidden";
     }
 
-    if (overlayItems[i].style.opacity === '1') {
+    if (overlayItems[i].style.opacity === "1") {
       overlayItems[i].style.opacity = 0;
-      overlayItems[i].style.visibility = 'hidden';
+      overlayItems[i].style.visibility = "hidden";
       activeIndex = null;
     } else {
       overlayItems[i].style.opacity = 1;
-      overlayItems[i].style.visibility = 'visible';
+      overlayItems[i].style.visibility = "visible";
       activeIndex = i;
     }
   });
 }
 
-document.addEventListener('click', (event) => {
-  const isOverlayClicked = Array.from(overlayItems).some((item) => item.contains(event.target));
-  const isProjectItemClicked = Array.from(projectItems).some((item) => item.contains(event.target));
+document.addEventListener("click", (event) => {
+  const isOverlayClicked = Array.from(overlayItems).some((item) =>
+    item.contains(event.target)
+  );
+  const isProjectItemClicked = Array.from(projectItems).some((item) =>
+    item.contains(event.target)
+  );
 
   if (!isOverlayClicked && !isProjectItemClicked) {
     Array.from(overlayItems).forEach((item) => {
       item.style.opacity = 0;
-      item.style.visibility = 'hidden';
+      item.style.visibility = "hidden";
     });
 
     activeIndex = null;
   }
 });
-
-
-
-
