27077fb0ca137b90d6b8d9f3f18bef307c8a2fea
Danila <93872886+DanyaGTx@users.noreply.github.com>
2025-07-08 09:26:15 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const downloadCV = () => {
   const resumeLink =
-    "https://drive.usercontent.google.com/uc?id=10s7SNHYRr5-DqFDX_vivlgzsJoPvoL-X&authuser=0&export=download";
+    "https://drive.usercontent.google.com/uc?id=1T2JqDQZSQ5VbvOfJVGSeULMiJYeD-9Up&authuser=0&export=download";
   window.location.href = resumeLink;
 };
 
